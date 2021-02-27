d7226a918c937d4eec2f95bbac125371a59f0c88
Siddhesh Kankekar <kankekas@tcd.ie>
2021-02-27 15:47:25 +0000

    Update styling on Login Page

--- src/Login.js
+++ src/Login.js
@@ -29,7 +29,11 @@ function Login() {
           src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
           alt=""
         />
-        <Button onClick={signIn}>LOGIN</Button>
+        <Button 
+        className="login__button"
+        variant="contained" color="primary" onClick={signIn}>
+          LOGIN
+        </Button>
       </div>
     </div>
   );
